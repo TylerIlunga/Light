a51f8e05cb365af864945c867b9c60c7fb48451f
Tyler Ilunga <tylerilunga@Tylers-MacBook-Pro.local>
2017-07-28 03:26:27 -0700

    Final

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var lightOn = true
+    
+    @IBOutlet weak var lightButton: UIButton!
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var instruction: UILabel!
+    
+    @IBAction func lightButtonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        update()
+    }
+    
+    
+    func update(){
+        if lightOn {
+            view.backgroundColor = .white
+            greetingLabel.textColor = .black
+            instruction.textColor = .black
+            greetingLabel.text = "Good Morning!"
+        } else {
+            view.backgroundColor = .black
+            greetingLabel.textColor = .white
+            instruction.textColor = .white
+            greetingLabel.text = "Good Evening!"
+        }
+    }
+    
+    func configureTaps() {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        update()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
